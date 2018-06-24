11ed7403aaf14752df25c1f361235125f8f3b51b
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-24 15:31:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,7 @@ class Application
 
     if req.path.match{/items/}
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.title == item_name}
-        if @@items.include?(item)
+        if item = @@items.find{|i| i.title == item_name}
           resp.write item.price
         else
           resp.write "Item not found"
