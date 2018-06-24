8c544443c53ad0b00c9cd6853b5109a4be8e81e7
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-24 15:33:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
     resp = Rack::Response.new
 
-    if req.path.match{/items/}
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last
         if item = @@items.find{|i| i.title == item_name}
           resp.write item.price
