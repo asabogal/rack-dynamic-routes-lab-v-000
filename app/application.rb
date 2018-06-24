ec102776749f8672352cb68aea05db5297ec0993
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-24 15:25:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,12 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.title == item_name}
       resp.write item.price
+
+    elsif !@@items.include?(item)
+      resp.write "Item not found"
+      resp.status = 404
+
+
     end
   end
 
